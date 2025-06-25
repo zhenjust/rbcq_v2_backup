2350ec2c1d80156aa134b640a939474697fc9b64
Jan Harold Dela Cruz <harolddc25@gmail.com>
2025-06-25 11:52:29 +0800

    chore: lint

---
+++ src/app/shared/constants/status.const.ts
@@ -0,0 +1,11 @@
+export enum MeterProcessStatus {
+    COMPLETED_METER_DATA = 'completed - meter data',
+    COMPLETED_SETTLEMENT_READY = 'completed - settlement ready',
+    COMPLETED_GESQ = 'completed - gesq'
+}
+
+export enum meterDataName {
+    SETTLEMENT_READY = 'settlementReady',
+    FINALIZE_READY = 'finalizeSettlementData',
+    INITIALIZE = 'initialize'
+}
