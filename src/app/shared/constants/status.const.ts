f459bff30043cafe0d98ef14e1b086b13aaa7987
Steven Siahetiong <ssiahetiong@exist.com>
2025-06-26 12:12:08 +0800

    chore: hide finalize

--- src/app/shared/constants/status.const.ts
+++ src/app/shared/constants/status.const.ts
@@ -5,15 +5,14 @@ export enum ProcessType {
 }
 
 export enum ProcessStatus {
-  COMPLETED = 'succeeded - ',
+  SUCCEEDED = 'succeeded - ',
   FAILED = 'failed - ',
   CANCELLED = 'cancelled - '
 }
 
 export enum MeterProcessStatus {
-  COMPLETED_METER_DATA = ProcessStatus.COMPLETED + ProcessType.METER_DATA,
-  COMPLETED_SETTLEMENT_READY = ProcessStatus.COMPLETED + ProcessType.SETTLEMENT_READY,
-  COMPLETED_GESQ = ProcessStatus.COMPLETED + ProcessType.GESQ,
+  SUCCEEDED_METER_DATA = ProcessStatus.SUCCEEDED + ProcessType.METER_DATA,
+  SUCCEEDED_GESQ = ProcessStatus.SUCCEEDED + ProcessType.GESQ,
   FAILED_SETTLEMENT_READY = ProcessStatus.FAILED + ProcessType.SETTLEMENT_READY,
   FAILED_GESQ = ProcessStatus.FAILED + ProcessType.GESQ,
   CANCELLED_SETTLEMENT_READY = ProcessStatus.CANCELLED + ProcessType.SETTLEMENT_READY,
