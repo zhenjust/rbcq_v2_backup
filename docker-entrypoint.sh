30b4cdd371462fc3d3c8b5a52d2f6611819ce5d5
Steven Siahetiong <ssiahetiong@exist.com>
2025-11-28 16:43:09 +0800

    chore: add replacing of env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Generate env.js from template at container start
-envsubst < /usr/share/nginx/html/crss/assets/env.template.ts > /usr/share/nginx/html/crss/assets/env.ts
+envsubst < /usr/share/nginx/html/crss/assets/env.template.js > /usr/share/nginx/html/crss/assets/env.js
 
 exec nginx -g 'daemon off;'
