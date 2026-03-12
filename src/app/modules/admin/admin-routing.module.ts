33c7d438e36d580df2597eb23c98344066d8b89a
Jason Doloso <jdoloso@exist.com>
2026-03-12 15:44:59 +0800

    feat: [IC-6106] wip: UI/UX: [Admin] Manage SEC Parameters

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageSecComponent } from './manage-sec/manage-sec.component';
-import { ngxPermissionsGuard } from 'ngx-permissions';
-import { externalRoutes, PHASE_TWO_AUTHORITIES } from '@shared/constants';
 
 const routes: Routes = [
   {
     path: 'sec',
     component: ManageSecComponent,
-    canActivate: [ngxPermissionsGuard],
-    data: {
-      permissions: {
-        only: [PHASE_TWO_AUTHORITIES.RUN_MQ_UPLOADER],
-        redirectTo: externalRoutes.HOME
-      }
-    },
+    // canActivate: [ngxPermissionsGuard],
+    // data: {
+    //   permissions: {
+    //     only: [PHASE_TWO_AUTHORITIES.RUN_MQ_UPLOADER],
+    //     redirectTo: externalRoutes.HOME
+    //   }
+    // },
   },
 
 ];
