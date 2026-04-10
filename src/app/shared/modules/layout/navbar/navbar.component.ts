a4b5eeb8a909306269c77747a7eda26d63c8adc4
Jason Doloso <jdoloso@exist.com>
2026-04-10 14:16:44 +0800

    feat: [IC-6073] wip: UI/UX: [Settlement][Additional Compensation] View Manage Additional Compensation Claim

--- src/app/shared/modules/layout/navbar/navbar.component.ts
+++ src/app/shared/modules/layout/navbar/navbar.component.ts
@@ -1261,15 +1261,12 @@ export class NavbarComponent implements OnInit {
     if (item.externalLink && item.externalLink.trim() !== '') {
       window.location.href = item.externalLink;
     } else if (item.path && item.path.trim() !== '') {
-      // console.log(item, item.path)
-      // const path = item.path.split('/');
-      // if (path.length > 1) {
-      // this.r.navigate(path);
-
-      // }
-      const cleanPath = item.path.replace(/^\/+/, '');
-      const segments = cleanPath.split('/');
-      this.r.navigate(segments, { relativeTo: this.r.routerState.root });    }
+      this.r.navigate([item.path]);
+    }
+      // const cleanPath = item.path.replace(/^\/+/, '');
+      // const segments = cleanPath.split('/');
+      // console.log()
+      // this.r.navigate(segments, { relativeTo: this.r.routerState.root });    }
   }
 
   toggleDropdown(item: navItems, isOpen: boolean): void {
