3d09138f4f97c46daafe73eb054ee6e1a150767a
Jason Doloso <jdoloso@exist.com>
2026-05-20 13:44:40 +0800

    feat: [IC-6912] wip:  UI/UX: [Settlement][EnergyMarketFee]Upload Billing Statements

--- src/app/shared/components/upload-billing-statement/upload-billing-statement.component.ts
+++ src/app/shared/components/upload-billing-statement/upload-billing-statement.component.ts
@@ -101,6 +101,8 @@ export class UploadBillingStatementComponent implements OnInit {
       formData.append(key, value as string);
     });
 
+    formData.append('billingPeriodId', '0');
+
     this.busy$ = this.settlementService.uploadBillingStatement(formData)
       .pipe(takeUntilDestroyed(this.destroyRef$))
       .subscribe(res => {
