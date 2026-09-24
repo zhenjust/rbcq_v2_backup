c0b893281bf03c18b1e2bafbe8d1fac11a048155
dev@iemop.ph <dev@iemop.ph>
2026-09-24 13:58:24 +0800

    to personal update the label

--- src/app/shared/constants/rbcq.const.ts
+++ src/app/shared/constants/rbcq.const.ts
@@ -2,8 +2,8 @@ import { RbcqProcessType } from "@shared/enums/rbcq.enum";
 import { rbcqProcessOptions } from "@shared/interfaces/rbcq.interface";
 
 export const RBCQ_PROCESS_TYPE: rbcqProcessOptions[] = [
-    {id: RbcqProcessType.INTIALIZE, label: 'Initialize', value: RbcqProcessType.INTIALIZE},
-    {id: RbcqProcessType.FINALIZE, label: 'Finalize', value: RbcqProcessType.FINALIZE},
+    {id: RbcqProcessType.INTIALIZE, label: 'Initialize RBCQ', value: RbcqProcessType.INTIALIZE},
+    {id: RbcqProcessType.FINALIZE, label: 'Finalize RBCQ', value: RbcqProcessType.FINALIZE},
     {id: RbcqProcessType.AP_FLAG, label: 'AP Flag', value: RbcqProcessType.AP_FLAG},
     {id: RbcqProcessType.ASIE_RESERVE, label: 'ASIE Reserve', value: RbcqProcessType.ASIE_RESERVE},
     {id: RbcqProcessType.ASIE_RESERVE_AP, label: 'ASIE Reserve AP', value: RbcqProcessType.ASIE_RESERVE_AP}
